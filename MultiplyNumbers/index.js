d9adea4e0a7dec27e42681b12e8601a33ee097f8
yuyuzeze <>
2023-09-01 17:55:34 +0900

    Query Parameter Check

--- MultiplyNumbers/index.js
+++ MultiplyNumbers/index.js
@@ -1,10 +1,10 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if(req.query.num1 && req.query.num2){
+    if(req.query.num1 && req.query.num2 && (!isNaN(req.query.num1) && !isNaN(req.query.num2))){
         context.res = {
             // status: 200
-            body: req.query.num1 * req.query.num2
+            body: "The Result is " + req.query.num1 * req.query.num2
         }
     }
     else{
